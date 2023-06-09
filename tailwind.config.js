e2a56f532edd051de546cb1c616fc534648093c1
denver154 <magildaceria@gmail.com>
2023-06-09 14:45:19 +0800

    second push

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,45 @@ module.exports = {
         dark:"#18406B",
         textcolor:"#2A374C",
         green:"#31AF36",
+        grayy:"#777777",
+        edupit:"#123A67",
       },
+      keyframes: {
+        sliding1:{
+          from: {
+            left:'0',
+          },
+          to: {
+            left:'100%',
+          },
+        },
+        sliding2:{
+          from: {
+            right:'0',
+          },
+          to: {
+            right:'100%',
+          },
+        }
+      },
+      animation:{
+        sliding1: 'sliding1 3s linear infinite',
+        sliding2: 'sliding2 3s linear infinite',
+      },
+
+      backgroundImage:{
+        'bannerbg': 'url("./img/slider__bg.jpg")',
+        'bannerimg1': 'url("./img/bannerlogo.jpg")',
+        'bannerimg2': 'url("./img/bannerlogo2)'
+      }
+
+
+
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
 
